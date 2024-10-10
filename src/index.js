1c2d6f4f818a853e06f5be16464a99b716774279
Fadziso Gabrielle <fadziso@novodigital.biz>
2024-10-10 23:15:48 +0200

    Modified Javascript

--- src/index.js
+++ src/index.js
@@ -18,6 +18,28 @@ brisbaneDateElement.innerHTML = brisbaneTime.format("MMMM do yyyy");
 brisbaneTimeElement.innerHTML = `${brisbaneTime.format('h:mm:ss')} <span>${brisbaneTime.format("A")}</span>` 
 }
 
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+   // console.log(cityTimeZone);
+    let cityName = cityTimeZone.replace("_", "").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+   // console.log(cityTime.format("MMMM Do YYYY"));
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+        <div class="container-display">
+            <div class="city-date">
+                <h2 class="city">${cityName}</h2>
+                <p class="date">${cityTime.format("MMMM Do YYYY")}</p>
+            </div>
+            <div>
+                <div class="time">${cityTime.format("h:mm:ss")}<span class="time-span">${cityTime.format("A")}</span></div>
+            </div>
+        </div>
+    `
+}
+
 updateTime();
 setInterval(updateTime, 1000);
 
+let citiesSelect = document.querySelector("#city");
+citiesSelect.addEventListener("change", updateCity);
